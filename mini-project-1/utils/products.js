dbbf7448f3d8a002c4b2f347437259309a484539
annkl <annkl@itu.dk>
2022-03-10 15:21:33 +0100

    product images renamed

--- mini-project-1/utils/products.js
+++ mini-project-1/utils/products.js
@@ -6,7 +6,7 @@ const productList = [
     price: 59,
     description: "Dark roasted beans for your delightful morning...",
     category: "Dark roast",
-    productImage: "../assets/product_images/cafe1.png",
+    productImage: "../assets/product_images/coffee1.png",
     isInCart: 0,
   },
   {
@@ -17,7 +17,7 @@ const productList = [
     description:
       "Enjoy our special blend of balanced and sweet flavours with herbal and maple notes made with the same high quality 100% Arabica beans.",
     category: "Medium roast",
-    productImage: "../assets/product_images/cafe2.png",
+    productImage: "../assets/product_images/coffee2.png",
     isInCart: 0,
   },
   {
@@ -28,7 +28,7 @@ const productList = [
     description:
       "Smooth with chocolate notes, Starbucks Pike Place Roast Whole Bean Coffee, ready to grind.",
     category: "Medium roast",
-    productImage: "../assets/product_images/cafe3.png",
+    productImage: "../assets/product_images/coffee3.png",
     isInCart: 0,
   },
   {
@@ -39,7 +39,7 @@ const productList = [
     description:
       "Lightly roasted Latin American coffee that is mellow and flavourful with a nice softness.",
     category: "Light roast",
-    productImage: "../assets/product_images/cafe4.png",
+    productImage: "../assets/product_images/coffee4.png",
     isInCart: 0,
   },
   {
@@ -50,7 +50,7 @@ const productList = [
     description:
       "Soft and balanced, it highlights milk's sweeter side, making classic espresso drinks extra smooth without a roasty edge.",
     category: "Light roast",
-    productImage: "../assets/product_images/cafe5.png",
+    productImage: "../assets/product_images/coffee5.png",
     isInCart: 0,
   },
   {
@@ -61,7 +61,7 @@ const productList = [
     description:
       "Aroma, body and flavor all in balance. House Blend is a medium roast with notes of cocoa and toffee.",
     category: "Medium roast",
-    productImage: "../assets/product_images/cafe6.png",
+    productImage: "../assets/product_images/coffee6.png",
     isInCart: 0,
   },
   {
@@ -72,7 +72,7 @@ const productList = [
     description:
       "Full-bodied with a smooth mouthfeel, lingering notes of dried herbs and fresh earth, and almost no acidity.",
     category: "Dark roast",
-    productImage: "../assets/product_images/cafe7.png",
+    productImage: "../assets/product_images/coffee7.png",
     isInCart: 0,
   },
   {
@@ -83,7 +83,7 @@ const productList = [
     description:
       "Dark hand-roasted organic espresso. Strong and fruity, with notes of citrus and almond and scents of caramel and chocolate.",
     category: "Dark roast",
-    productImage: "../assets/product_images/depEpisk.png",
+    productImage: "../assets/product_images/coffee8.png",
     isInCart: 0,
   },
   {
@@ -94,7 +94,7 @@ const productList = [
     description:
       "Medium hand-roasted coffee from Central America. Deliciously creamy and smooth with well-balanced acidity. Notes of nuts and chocolate. Tastes Magical.",
     category: "Medium roast",
-    productImage: "../assets/product_images/depMagisk.png",
+    productImage: "../assets/product_images/coffee9.png",
     isInCart: 0,
   },
   {
@@ -105,7 +105,7 @@ const productList = [
     description:
       "Medium roasted rounded and creamy coffee with a light note of spice. Smooth and creamy Java with a hint of citrus from Ethiopia.",
     category: "Medium roast",
-    productImage: "../assets/product_images/depSympati.png",
+    productImage: "../assets/product_images/coffee10.png",
     isInCart: 0,
   },
   {
@@ -116,7 +116,7 @@ const productList = [
     description:
       "Medium hand-roasted organic coffee from Honduras. Fruity with notes of nuts and chocolate.",
     category: "Medium roast",
-    productImage: "../assets/product_images/depReboot.png",
+    productImage: "../assets/product_images/coffee11.png",
     isInCart: 0,
   },
   {
@@ -127,7 +127,7 @@ const productList = [
     description:
       "Medium hand-roasted organic coffee from Peru. Formidable strong flavoured, with low-acidity and scent of caramel and fresh tobacco leaves.",
     category: "Medium roast",
-    productImage: "../assets/product_images/depQuickFix.png",
+    productImage: "../assets/product_images/coffee12.png",
     isInCart: 0,
   },
   {
@@ -138,7 +138,7 @@ const productList = [
     description:
       "Oro is something special. Oro is the original Luigi Lavazza blend and thus the cornerstone of Lavazza's passion for coffee. The intense aroma of coffee has made it a favorite classic among passionate coffee drinkers and connoisseurs. The coffee is one of the finest 100% Arabica beans from Central America and the African highlands. Oro is full-bodied, soft and aromatic with a round refined taste.",
     category: "Medium roast",
-    productImage: "../assets/product_images/Coffee13.png",
+    productImage: "../assets/product_images/coffee13.png",
     isInCart: 0,
   },
   {
@@ -149,7 +149,7 @@ const productList = [
     description:
       "100% organic and sustainable quality coffee hand-picked in East Africa. The coffee is full-bodied and intense, which combines Arabica and Robusta in a wonderful combination - intensity 7/10. Lavazza iTierra! For Africa - The Lavazza Foundation supports the next generation of farmers through entrepreneurship and management training.",
     category: "Dark roast",
-    productImage: "../assets/product_images/Coffee14.png",
+    productImage: "../assets/product_images/coffee14.png",
     isInCart: 0,
   },
   {
@@ -160,7 +160,7 @@ const productList = [
     description:
       "Full-bodied and well-balanced espresso. The carefully selected coffee beans, and the gentle roasting, give the coffee characteristic notes of honey and roasted nuts. The aroma is unsurpassed and the texture of the coffee is rich and intense. With L’Espresso Gran Aroma, you feel like a barista in your own home. Because with L’Espresso Gran Aroma, you can achieve the taste of a traditional Italian coffee bar, in your own cozy and homely setting.",
     category: "Dark roast",
-    productImage: "../assets/product_images/Coffee15.png",
+    productImage: "../assets/product_images/coffee15.png",
     isInCart: 0,
   },
   {
@@ -171,7 +171,7 @@ const productList = [
     description:
       "Rossa is popular and the best-selling whole bean in Italy. That says a lot about the quality of a coffee. Rossa combines Arabica and Robusta beans from Brazil and Africa in an exquisite blend. The coffee is pleasant and full-bodied with an intense aroma and a taste of chocolate. The taste experience is pleasant and with an insistent finish. The rounded and rich taste of the coffee explains the popularity.",
     category: "Medium roast",
-    productImage: "../assets/product_images/Coffee16.png",
+    productImage: "../assets/product_images/coffee16.png",
     isInCart: 0,
   },
   {
@@ -182,7 +182,7 @@ const productList = [
     description:
       "100% organic and sustainable quality coffee hand-picked in South America. The coffee is balanced and aromatic with notes of chocolate - intensity 5/10. Lavazza iTierra! For Amazonia - Lavazza Foundation continuously helps replant new rainforests in the region and protects the Amazon's environmental heritage.",
     category: "Light roast",
-    productImage: "../assets/product_images/Coffee17.png",
+    productImage: "../assets/product_images/coffee17.png",
     isInCart: 0,
   },
   {
@@ -193,7 +193,7 @@ const productList = [
     description:
       "Dark hand-roasted beans. Dark and creamy espresso with a hint of sweetness and low-acidity. Lovely notes of chocolate and a scent of nuts.",
     category: "Dark roast",
-    productImage: "../assets/product_images/depBoost.png",
+    productImage: "../assets/product_images/coffee18.png",
     isInCart: 0,
   },
   {
@@ -204,7 +204,7 @@ const productList = [
     description:
       "Tierra Organic is an exquisite organic blend, where only the best beans are selected consisting of 100% organic and sustainable Arabica beans grown in UTZ certified areas. The coffee is lovingly roasted according to authentic Italian tradition to achieve the perfect combination of taste and aroma. The coffee is round and balanced with fine floral and fruity undertones. Tierra! Organic is for you who want to enjoy an exquisite organic coffee and at the same time help to take good care of the environment. 100% Organic, 100% Sustainable, 100% Arabica",
     category: "Dark roast",
-    productImage: "../assets/product_images/Coffee19.png",
+    productImage: "../assets/product_images/coffee19.png",
     isInCart: 0,
   },
   {
@@ -215,7 +215,7 @@ const productList = [
     description:
       "Notes of sweet orange and brown sugar mingle in our lightest medium roast coffee.",
     category: "Medium roast",
-    productImage: "../assets/product_images/cafe20.png",
+    productImage: "../assets/product_images/coffee20.png",
     isInCart: 0,
   },
 ];
