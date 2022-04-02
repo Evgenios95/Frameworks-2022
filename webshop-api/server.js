b9c45bdd3fade67084a8007083f486e819dedd30
Michal Dang <michaldang99@gmail.com>
2022-04-02 13:56:01 +0200

    stuff

--- webshop-api/server.js
+++ webshop-api/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const basketRouter = require('./routes/basket.js')
+
+app.use('/basket', basketRouter);
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`Example      app listening on port ${port}`);
+    console.log(`${port}`);
 });
 
