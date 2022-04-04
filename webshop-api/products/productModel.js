b49875914892e36b409a2e5232bd9c948a139249
Michal Dang <michaldang99@gmail.com>
2022-04-04 15:28:20 +0200

    call updates

--- webshop-api/products/productModel.js
+++ webshop-api/products/productModel.js
@@ -3,38 +3,37 @@ import fs from "fs/promises";
 const PRODUCTS_FILE = "./products/products.json";
 
 export async function getAllProducts() {
-  try {
-    let products = await fs.readFile(PRODUCTS_FILE);
-    let parsedProducts = JSON.parse(products);
-    return parsedProducts;
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      return [];
-    } else throw err;
-  }
+    try {
+        let products = await fs.readFile(PRODUCTS_FILE);
+        return JSON.parse(products);
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            return [];
+        } else throw err;
+    }
 }
 
 export async function getProductById(id) {
-  let products = await getAllProducts();
-  return products[parseInt(id) - 1];
+    let products = await getAllProducts();
+    return products[parseInt(id) - 1];
 }
 
 export async function getFilteredProducts(queryCategory) {
-  try {
-    let products = await getAllProducts();
+    try {
+        let products = await getAllProducts();
 
-    const filteredProducts = products.filter((product) => {
-      const filteredCategory = Object.values(product.productCategories).find(
-        (category) => category === queryCategory
-      );
+        const filteredProducts = products.filter((product) => {
+            const filteredCategory = Object.values(product.productCategories).find(
+                (category) => category === queryCategory
+            );
 
-      return filteredCategory;
-    });
+            return filteredCategory;
+        });
 
-    return filteredProducts;
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      return [];
-    } else throw err;
-  }
+        return filteredProducts;
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            return [];
+        } else throw err;
+    }
 }
--- webshop-api/products/productRouter.js
+++ webshop-api/products/productRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {
-  allProducts,
-  filteredProducts,
-  productById,
+    allProducts,
+    filteredProducts,
+    productById,
 } from "./productController.js";
 
 const router = express.Router();
+
 router.route("/all").get(allProducts);
 router.route("/filtered").get(filteredProducts);
 router.route("/:id").get(productById);
