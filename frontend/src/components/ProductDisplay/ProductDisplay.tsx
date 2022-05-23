9a78e7edb06ca542226608d9d68fff24d94f7af1
evgenios95 <evgeniospam@gmail.com>
2022-05-23 09:28:16 +0200

    Implemented pagination + scroll to top button

--- frontend/src/components/ProductDisplay/ProductDisplay.tsx
+++ frontend/src/components/ProductDisplay/ProductDisplay.tsx
@@ -1,15 +1,21 @@
 import "./style.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Product } from "../Product";
 import { motion } from "framer-motion";
 import NoResultImg from "../../assets/coffee.gif";
 import { ProductProps } from "../Product/Product";
+import useProductsPagination from "../../utils/hooks/useProductsPagination";
+import { Pagination } from "@mui/material";
+import { useLocation } from "react-router";
+import { ScrollButton } from "../ScrollButton";
 
 interface ProductDisplayProps {
   products: ProductProps[];
 }
 
 export const ProductDisplay = ({ products }: ProductDisplayProps) => {
+  let [page, setPage] = useState(1);
+
   // Check more information about framer-motion
   // https://www.framer.com/docs/animation/
   const containerAnim = {
@@ -24,6 +30,36 @@ export const ProductDisplay = ({ products }: ProductDisplayProps) => {
     },
   };
 
+  useEffect(() => {
+    paginatedProducts.jump(1);
+    setPage(1);
+
+    // eslint-disable-next-line
+  }, [products]);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [page]);
+
+  const productCount = Math.ceil(products.length / 9);
+  const paginatedProducts: any = useProductsPagination(products, 9);
+  const handleChange = (e: React.ChangeEvent<unknown>, p: number) => {
+    setPage(p);
+    paginatedProducts.jump(p);
+  };
+
+  const location = useLocation();
+
+  const shownProducts =
+    location.pathname === "/products"
+      ? paginatedProducts.currentProducts()
+      : products;
+
+  const isProductsPage = location.pathname === "/products";
+
   return (
     <>
       <motion.div
@@ -32,10 +68,23 @@ export const ProductDisplay = ({ products }: ProductDisplayProps) => {
         initial="hidden"
         animate="show"
       >
-        {products.map((product: ProductProps) => (
+        {shownProducts.map((product: ProductProps) => (
           <Product key={product.productId} product={product} />
         ))}
       </motion.div>
+
+      {isProductsPage && (
+        <Pagination
+          count={productCount}
+          size="large"
+          page={page}
+          variant="outlined"
+          shape="rounded"
+          onChange={handleChange}
+        />
+      )}
+
+      <ScrollButton />
 
       {products.length === 0 && (
         <div className={"no-products-message"}>
